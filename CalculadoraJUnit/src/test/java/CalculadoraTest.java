05a898e19b3f3e677954598d3dbd9e24ff3df246
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2025-06-19 14:42:35 -0300

    Commit Inicial Soma
    Adicionado Função Soma da Calculadora

---
+++ CalculadoraJUnit/src/test/java/CalculadoraTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+
+    Calculadora calc = new Calculadora();
+
+    @Test
+    void testSoma() {
+        assertEquals(10, calc.somar(7, 3));
+        assertEquals(-2, calc.somar(-5, 3));
+        assertEquals(0, calc.somar(0, 0));
+    }
+//
+//    @Test
+//    void testSubtrair() {
+//        assertEquals(4, calc.subtrair(7, 3));
+//        assertEquals(-8, calc.subtrair(-5, 3));
+//        assertEquals(0, calc.subtrair(0, 0));
+//    }
+//
+//    @Test
+//    void testMultiplicar() {
+//        assertEquals(21, calc.multiplicar(7, 3));
+//        assertEquals(-15, calc.multiplicar(-5, 3));
+//        assertEquals(0, calc.multiplicar(0, 5));
+//    }
+//
+//    @Test
+//    void testDividir() {
+//        assertEquals(2, calc.dividir(6, 3));
+//        assertEquals(-2, calc.dividir(-6, 3));
+//        assertThrows(IllegalArgumentException.class, () -> calc.dividir(5, 0));
+//    }
+}
